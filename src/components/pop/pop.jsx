14d1d7ad184c2dca94fa6a1a2d1833543973cc79
yubaoquan <hzyubaoquan@corp.netease.com>
2018-12-19 11:21:42 +0800

    prepare for pop

--- src/components/pop/pop.jsx
+++ src/components/pop/pop.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Base from '../base';
+import './style';
 
 class Pop extends Base {
   render() {
-    return <span className="shiye-pop">pop</span>
+    return <span className="shiye-pop">-pop-</span>
   }
 }
 
