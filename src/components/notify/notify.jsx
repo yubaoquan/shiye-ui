22cb7c456f221b17e93ba2d61c19d6b5e6158a6c
yubaoquan <hzyubaoquan@corp.netease.com>
2018-12-23 22:37:48 +0800

    first notify done

--- src/components/notify/notify.jsx
+++ src/components/notify/notify.jsx
@@ -1,11 +1,98 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Base from '../base';
-import PropTypes from 'prop-types';
+import Toast from './toast';
+import classNames from 'classnames';
+import './style';
 
 class Notify extends Base {
+
+  state = {
+    finished: false,
+  }
+
+  static createRootMountPoint() {
+    const el = Notify.el = document.createElement('span');
+    el.className = 'shiye-notify-container';
+    document.body.appendChild(el);
+  }
+
+  static createMountPoint() {
+    const el = document.createElement('span');
+    Notify.el.appendChild(el);
+    return el;
+  }
+
+  static createToast(
+    text = Notify.config.text,
+    duration = Notify.config.duration,
+    cb,
+    type = 'success',
+  ) {
+    if (!Notify.el) {
+      Notify.createRootMountPoint();
+    }
+
+    const mountPoint = Notify.createMountPoint();
+    const id = Math.random();
+    const toast = {
+      id,
+      text,
+      type,
+      duration,
+      cb,
+      index: Notify.toasts.length,
+    };
+
+    Notify.toasts.push({ id, mountPoint });
+    const instance = <Notify toast={toast} />;
+    ReactDOM.render((<>{instance}</>), mountPoint);
+  }
+
+  static error(text, duration, cb) {
+    Notify.createToast(text, duration, cb, 'error');
+  }
+
+  static success(text, duration, cb) {
+    Notify.createToast(text, duration, cb, 'success');
+  }
+
+  static config(cfg) {
+    Object.assign(Notify.config, cfg);
+  }
+
+  static clear() {
+    Notify.toasts.forEach(({ mountPoint }) => {
+      Notify.el.removeChild(mountPoint);
+    });
+    Notify.toasts = [];
+  }
+
+  static remove(id) {
+    const item = Notify.toasts.find(item => item.id === id);
+    Notify.el.removeChild(item.mountPoint);
+    Notify.toasts = Notify.toasts.filter(item => item.id !== id);
+  }
+
+  onAnimationEnd = (id) => {
+    Notify.remove(id);
+    this.setState({ finished: true });
+  }
+
+  static instance = null
+  static toasts = []
+
+  static config = {
+    duration: 2000,
+  }
+
   render() {
-    return (
-      <div>building notify</div>
+    if (this.state.finished) {
+      return null;
+    }
+    return ReactDOM.createPortal(
+      <Toast {...this.props.toast} onAnimationEnd={this.onAnimationEnd} />,
+      Notify.el
     );
   }
 }
---
+++ src/components/notify/toast.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Base from '../base';
+import classNames from 'classnames';
+
+class Toast extends Base {
+
+  static propTypes = {
+    text: PropTypes.node.isRequired,
+    type: PropTypes.oneOf(['success', 'error']),
+    duration: PropTypes.number,
+    cb: PropTypes.func,
+    onAnimationEnd: PropTypes.func,
+    id: PropTypes.string,
+  }
+
+  static defaultProps = {
+    text: '',
+    duration: 1000,
+    type: 'success',
+  }
+
+  state = { goOut: false }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ goOut: true });
+    }, this.props.duration);
+  }
+  onAnimationEnd = () => {
+    if (this.state.goOut) {
+      this.safeCall(this.props.onAnimationEnd, [this.props.id]);
+    }
+  }
+
+  render() {
+    const {
+      text,
+      type,
+    } = this.props;
+    const { goOut } = this.state;
+
+    const cn = classNames(
+      this.prefixClass('-notify-toast'),
+      this.prefixClass(`-notify-toast-${type}`),
+      {
+        [this.prefixClass('-notify-toast-out')]: goOut,
+      }
+    );
+
+    return (
+      <div
+        className={cn}
+        onAnimationEnd={this.onAnimationEnd}
+      >{text}</div>
+    );
+  }
+}
+
+export default Toast;
